d852c554bb7557c284b5929b0882e6fde2469803
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-07 16:51:00 +0530

    new

---
+++ Customreact/customreact.js
@@ -0,0 +1,20 @@
+function customrender(reactElement, Container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.Children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+
+  Container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blanck",
+  },
+  Children: "click me to visit google ",
+};
+
+const mainContainer = document.querySelector("#root");
+customrender(reactElement, mainContainer);
