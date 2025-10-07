d7896b5338e534fcede845c7afd0bb4d51bf0ab5
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-07 15:58:20 +0530

    react with basic react

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,12 @@
+import Chai from "./chai";
+
 function App() {
-  return <h1>hello world </h1>;
+  return (
+    <>
+      <h1>hello world </h1>
+      <Chai />
+    </>
+  );
 }
 
 export default App;
