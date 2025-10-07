aec71d7994057ad473a08f733d13aba1cfc89730
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-07 19:14:25 +0530

    new

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import React from "react";
 
-const reactElement = React.createElement(
-  "a",
-  {
-    href: "https://google.com",
-    target: "click me to visit google",
-  },
-  "click me to go to google"
-);
+// const reactElement = React.createElement(
+//   "a",
+//   {
+//     href: "https://google.com",
+//     target: "click me to visit google",
+//   },
+//   "click me to go to google"
+// );
 
-ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
