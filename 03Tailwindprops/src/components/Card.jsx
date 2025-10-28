dc54efa85fa29f221f7704089b0035f9a7defd0e
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-28 15:54:58 +0530

    tailwind

---
+++ 03Tailwindprops/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Card({ Username, userinfo }) {
+  // console.log("props", props);
+  console.log(Username);
+
+  return (
+    <div className="flex flex-col bg-white border border-gray-200 shadow-2xs rounded-xl">
+      <img
+        className="w-full h-auto rounded-t-xl"
+        src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&q=80"
+        alt="Card"
+      />
+      <div className="p-4 md:p-5">
+        <h3 className="text-lg font-bold text-gray-800">{Username}</h3>
+        <p className="mt-1 text-gray-500">{userinfo}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
