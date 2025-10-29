9a2235349a1c823d5fdd486285e9d769e4b0df3e
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-29 17:18:21 +0530

    new

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -5,42 +5,37 @@ function App() {
 
   return (
     <div className="w-screen h-screen" style={{ backgroundColor: Color }}>
-      <div className="fixed flex flex-wrap justify-center bottom-12 rounded-xl inset-x-0  gap-40 bg-blue-950 ">
-        <button
-          onClick={() => setColor("orange")}
-          className="p-4"
-          style={{ backgroundColor: "orange " }}
-        >
-          orange
-        </button>
-        <button
-          onClick={() => setColor("violet")}
-          className=" p-4"
-          style={{ backgroundColor: "violet " }}
-        >
-          violet
-        </button>
-        <button
-          onClick={() => setColor("green")}
-          className=" p-4"
-          style={{ backgroundColor: "green " }}
-        >
-          green
-        </button>
-        <button
-          onClick={() => setColor("black")}
-          className=" p-4"
-          style={{ backgroundColor: "black " }}
-        >
-          black
-        </button>
-        <button
-          onClick={() => setColor("pink")}
-          className=" p-4"
-          style={{ backgroundColor: "pink " }}
-        >
-          pink
-        </button>
+      <div className="fixed flex flex-wrap justify-center bottom-12 rounded-xl inset-x-0  ">
+        <div className="flex flex-wrap justify-center px-20 rounded-xl inset-x-0 gap-40 bg-blue-950 ">
+          <button
+            onClick={() => setColor("orange")}
+            className="p-4"
+            style={{ backgroundColor: "orange " }}
+          >
+            orange
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className=" p-4"
+            style={{ backgroundColor: "green " }}
+          >
+            green
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className=" p-4"
+            style={{ backgroundColor: "black " }}
+          >
+            black
+          </button>
+          <button
+            onClick={() => setColor("pink")}
+            className=" p-4"
+            style={{ backgroundColor: "pink " }}
+          >
+            pink
+          </button>
+        </div>
       </div>
     </div>
   );
