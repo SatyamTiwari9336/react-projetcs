7e03531043e42f0e9f22317deb6aabae7ec091bb
SatyamTiwari9336 <satyamtiwarisat@gmail.com>
2025-10-29 15:01:21 +0530

    new cahnge

--- PlayerCard/src/App.jsx
+++ PlayerCard/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
     <div>
       <NewCard matches={20} won={12} lost={8} events={6} rank={22} />
       <NewCard matches={30} won={24} lost={6} events={13} rank={1} />
-      <NewCard matches={1} won={1} lost={0} events={0} rank={"-"} />
     </div>
   );
 }
